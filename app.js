2509d0d1a06a3a928f7cbbfb0d1dae7aca7d5d40
AlexPenkin <Sanekpenkin@gmail.com>
2016-06-12 00:44:51 +0300

    beautify

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var app = require('express')();
 var path = require("path");
-var server = app.listen('8888','192.168.0.101', function (req, res) {
+var server = app.listen('8888', '192.168.0.101', function(req, res) {
   console.log('Server on 8888');
 });
 app.use(require("express").static(path.join(__dirname, 'public')));
-app.get('/', function (req, res, next) {
+app.get('/', function(req, res, next) {
   res.send('./public/index.html')
 })
