fd6fd194b6a13e5f22da20ebdf8b7088ff1add01
Alexander Penkin <a.penkin@smartcodes.ru>
2016-06-14 17:52:52 +0300

    Armenians

---
+++ public/js/js.js
@@ -0,0 +1,17 @@
+ymaps.ready(init);
+var myMap,
+  myPlacemark;
+
+function init() {
+  myMap = new ymaps.Map("map", {
+    center: [54.730505, 39.461868],
+    zoom: 14
+  });
+
+  myPlacemark = new ymaps.Placemark([54.730505, 39.461868], {
+    hintContent: 'Москва!',
+    balloonContent: 'Столица России'
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+}
