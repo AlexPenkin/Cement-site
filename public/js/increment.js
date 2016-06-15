12e911be06fd235f519ab22c5e6c263c2d621d83
Alexander Penkin <a.penkin@smartcodes.ru>
2016-06-15 13:20:06 +0300

    commit

--- public/js/increment.js
+++ public/js/increment.js
@@ -1,7 +1,7 @@
     $(document).ready(function() {
       var element_position = $('.n').offset().top - 600;
       var screen_height = $(window).height();
-      var activation_offset = 0.5; 
+      var activation_offset = 0.5;
       var activation_point = element_position - (screen_height * activation_offset);
       var notCompleted = true;
       var max_scroll_height = $('body').height() - screen_height - 5; //-5 for a little bit of buffer
@@ -17,11 +17,11 @@
           notCompleted = false;
           $(".w").spincrement({
             duration: 4000,
-            easing: 'easeInSine'
+            easing: 'easeInOutQuint'
           });
           $(".years").spincrement({
-            duration: 2000,
-            easing: 'easeInSine'
+            duration: 3500,
+            easing: 'easeInOutQuint'
           });
 
         }
