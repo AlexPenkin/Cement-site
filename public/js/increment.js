10039d9443e26043364b584d98adf242603f300c
Alexander Penkin <a.penkin@smartcodes.ru>
2016-06-15 11:39:29 +0300

    increment

---
+++ public/js/increment.js
@@ -0,0 +1,29 @@
+    $(document).ready(function() {
+      var element_position = $('.n').offset().top - 600;
+      var screen_height = $(window).height();
+      var activation_offset = 0.5; 
+      var activation_point = element_position - (screen_height * activation_offset);
+      var notCompleted = true;
+      var max_scroll_height = $('body').height() - screen_height - 5; //-5 for a little bit of buffer
+
+
+      $(window).on('scroll', function() {
+        var y_scroll_pos = window.pageYOffset;
+
+        var element_in_view = y_scroll_pos > activation_point;
+        var has_reached_bottom_of_page = max_scroll_height <= y_scroll_pos && !element_in_view;
+
+        if (element_in_view && notCompleted) {
+          notCompleted = false;
+          $(".w").spincrement({
+            duration: 4000,
+            easing: 'easeInSine'
+          });
+          $(".years").spincrement({
+            duration: 2000,
+            easing: 'easeInSine'
+          });
+
+        }
+      });
+    });
