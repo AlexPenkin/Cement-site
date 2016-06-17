d6bce97f86cd924b65d4760441f578a05ff66b89
Alexander Penkin <a.penkin@smartcodes.ru>
2016-06-17 14:14:29 +0300

    Almost done

--- public/js/sendMessage.js
+++ public/js/sendMessage.js
@@ -2,32 +2,32 @@ $(document).ready(function() {
 
   $(document).on('click', '.makeBidBut', function(event) {
     var self = $(this);
-   event.stopPropagation();
-   name =  $(self).siblings('.makeBidInp1').val(),
-   email =  $(self).siblings('.makeBidInp2').val(),
-   text = $(self).siblings('.makeBidInp3').val();
-      $.ajax({
-        method: "POST",
-        url: "send_mail.php",
-        data: {
-          action: "message",
-          name: name,
-          email: email,
-          text: text
-        },
-        success: function(json){
-         if (json['success'] == true) {
-           $('.onError').hide();
-           $('.order').addClass('messageSent');
-           $('.order').html('Сообщение отправлено!')
-            $('.order').removeClass('.order');
-         } else {
-           alert('Произошла ошибка!\nПроверьте, все ли поля заполнены правильно');
-           //$('.onError').show();
-           $('.onError').html('Проверьте, все ли поля заполнены правильно.');
-         }
-     }
-      })
-        });
+    event.stopPropagation();
+    name = $(self).siblings('.makeBidInp1').val(),
+      email = $(self).siblings('.makeBidInp2').val(),
+      text = $(self).siblings('.makeBidInp3').val();
+    $.ajax({
+      method: "POST",
+      url: "send_mail.php",
+      data: {
+        action: "message",
+        name: name,
+        email: email,
+        text: text
+      },
+      success: function(json) {
+        if (json['success'] == true) {
+          $('.onError').hide();
+          $('.order').addClass('messageSent');
+          $('.order').html('Сообщение отправлено!')
+          $('.order').removeClass('.order');
+        } else {
+          alert('Произошла ошибка!\nПроверьте, все ли поля заполнены правильно');
+          //$('.onError').show();
+          $('.onError').html('Проверьте, все ли поля заполнены правильно.');
+        }
+      }
+    })
+  });
 
 });
